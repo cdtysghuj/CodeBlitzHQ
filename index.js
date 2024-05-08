7bf00ea5efbed1c07358164828965c0eb183e98c
cdtysghuj <dsjcannyboy2427@hotmail.com>
2024-05-08 08:45:54 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-function minStack() {
-  this.stack = [];
-  this.minStack = [];
+function getPermutation(n, k) {
+  const nums = Array.from({ length: n }, (_, i) => i + 1);
+  let result = "";
+  let count = 0;
+  const backtrack = (current) => {
+    if (current.length === n) {
+      count++;
+      if (count === k) result = current.join("");
+      return;
+    }
+    if (count >= k) return;
+    for (const num of nums) {
+      if (current.includes(num)) continue;
+      current.push(num);
+      backtrack(current);
+      current.pop();
+    }
+  };
+  backtrack([]);
+  return result;
 }
-minStack.prototype.push = function (x) {
-  this.stack.push(x);
-  if (
-    this.minStack.length === 0 ||
-    x <= this.minStack[this.minStack.length - 1]
-  )
-    this.minStack.push(x);
-};
-minStack.prototype.pop = function () {
-  if (this.stack.pop() === this.minStack[this.minStack.length - 1])
-    this.minStack.pop();
-};
-minStack.prototype.top = function () {
-  return this.stack[this.stack.length - 1];
-};
-minStack.prototype.getMin = function () {
-  return this.minStack[this.minStack.length - 1];
-};
