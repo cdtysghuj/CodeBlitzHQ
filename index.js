a1d8a7a75393b501bea609e3cd9c3fdbfa6ba84c
cdtysghuj <dsjcannyboy2427@hotmail.com>
2024-05-11 00:15:16 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const zip = (arr1, arr2) => arr1.map((el, i) => [el, arr2[i]]);
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
+  }
+  return i === s.length;
+}
